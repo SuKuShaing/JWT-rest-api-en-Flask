f8f27b1e2276779f08bcddade961921ffab2f3b6
Sebastian <sebastian.sanhueza.t@usach.cl>
2024-10-29 20:59:34 -0300

    ahora tiene rutas de autentificación

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from routes.auth import routes_auth
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
+app.register_blueprint(routes_auth, url_prefix="/api")
+
+if __name__ == "__main__":
     app.run(debug=True, port="4000", host="0.0.0.0")
