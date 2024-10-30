6f996018e5c1fe8788c68f5be15f1b60d9a3d99b
Sebastian <sebastian.sanhueza.t@usach.cl>
2024-10-29 22:00:13 -0300

    se añade middleware para verificación de token en la ruta de usuarios de GitHub

--- routes/user_github.py
+++ routes/user_github.py
@@ -1,7 +1,24 @@
 from flask import Blueprint, request
 from requests import get
+from funtion_jwt import valida_token
 
 users_github = Blueprint("users_github", __name__)
+
+
+# Middleware
+@users_github.before_request
+def verify_token_middleware():
+    """
+    Solo sí la solicitud tiene un token valido lo deja pasar
+    """
+
+    token = request.headers["Authorization"].split(" ")[1]
+    valida_token(token, output=False)
+
+    """
+    No queremos que retorne nada si está todo bien, que siga su camino a la ruta de /github/users, 
+    sí va mal nos retorna un error
+    """
 
 
 @users_github.route("/github/users", methods=["POST"])
@@ -11,4 +28,6 @@ def github():
     """
     data = request.get_json()
     country = data["country"]
-    return get(f'https://api.github.com/search/users?q=location:"{country}"&page=1').json()
+    return get(
+        f'https://api.github.com/search/users?q=location:"{country}"&page=1'
+    ).json()
